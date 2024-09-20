ed82da276d620330cb59a53c5bd209509848e85e
Ethan Mcintyre <mcintyea@mail.uc.edu>
2024-09-20 09:05:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double Summercost = 120.10;
+        double Fallcost = 300.50;
+        double wintercost = 130.20;
+        double springcost = 145.00;
+        double totalCost;
+
+        totalCost = Summercost + Fallcost + wintercost + springcost;
+
+        System.out.println("The summer cost is: " + Summercost);
+        System.out.println("The Fallcost is: " + Fallcost);
+        System.out.println("The wintercost is: " + wintercost);
+        System.out.println("The springcost is: " + springcost);
+        System.out.println("The total yearly maintanance cost is " + totalCost);
+
+
+
+
+
+    }
+}
